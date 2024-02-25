c1643ab75d8c0885be2c96176c3e813cc8ae5bf5
Ibeh Miracle <miradmayor@gmail.com>
2024-02-25 18:27:49 -0400

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import Programs from './components/Programs/Programs'
+import Title from './components/Title/Title'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Title />
+        <Programs />
+      </div>
+      
     </div>
   )
 }
